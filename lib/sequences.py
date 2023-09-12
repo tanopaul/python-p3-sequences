4ce894c7dbf22de8f2b2e1f10385e0414e443e28
Tano Paul <tanopaul38@gmail.com>
2023-09-12 09:46:04 -0400

    completed python sequenceing

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib = []
+    for i in range(length):
+        if len(fib) == 0:
+            fib.append(i)
+        elif len(fib) == 1:
+            fib.append(i)
+        else:
+            num = fib[i - 2] + fib[i - 1]
+            fib.append(num)
+    print(fib)
+
+print_fibonacci(9)
